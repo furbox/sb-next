7a897112d4b095ae6b8b921ac59ed1d05f85b918
furbox <furbox@gmail.com>
2021-09-22 18:18:14 -0500

    init

---
+++ layouts/BasicLayout/BasicLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+export default function BasicLayout({children}) {
+    return (
+        <Container fluid className="basic-layout">
+            <Container className="content">
+                {children}
+            </Container>
+        </Container>
+    )
+}
